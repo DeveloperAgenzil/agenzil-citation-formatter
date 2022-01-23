422a6ec96e5034a4c0766320a9641596f17a69dc
DeveloperAgenzil <agenziles@gmail.com>
2022-01-23 23:15:45 +0100

    2 files modified, 1 added, 1 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component, VERSION } from '@angular/core';
+import { Component } from '@angular/core';
+import { Iitem } from './selection.interface';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  public selectedItem: number;
+
+  public items: Array<Iitem> = [
+    { id: 1, name: 'YouTube video' },
+    { id: 2, name: 'Book: no auth.' },
+  ];
 }
