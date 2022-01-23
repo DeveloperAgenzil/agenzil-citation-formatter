85b3bfa6b250e0137e88a39f6fa5f8b50439f7f2
DeveloperAgenzil <agenziles@gmail.com>
2022-01-23 23:43:12 +0100

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,19 @@ import { Iitem } from './selection.interface';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  public selectedItem: number;
+  public selectedItemId: number = 0;
+
+  public ResetSelectedItemId() {
+    this.selectedItemId = 0;
+  }
 
   public items: Array<Iitem> = [
+    { id: 0, name: '' },
     { id: 1, name: 'YouTube video' },
     { id: 2, name: 'Book: no auth.' },
   ];
+
+  public OnDifferentItemSelected() {
+    console.log(this.selectedItemId);
+  }
 }
